42f61eb67f15bfff5a668bfb1badfc3c8227d005
nkiruemina <40235847+nkiruemina@users.noreply.github.com>
2018-06-26 11:42:07 +0100

    Add files via upload

--- backend/db.php
+++ backend/db.php
@@ -1,8 +1,8 @@
 <?php
     $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db = 'proj_emsc';
+    $user = 'emsc_app_user';
+    $pass = 'emsc_app_password+77';
+    $db = 'emsc_db';
 
     $conn = mysqli_connect($host, $user, $pass, $db) or die ('Could not connect !');
 ?>
